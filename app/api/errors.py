37891d57f3a866ac507a730d0ea19bade22f782b
Răzvan-Petru Leonte <77784863+DonPiero@users.noreply.github.com>
2025-11-20 16:21:09 +0200

    Update errors.py
    Enforced error functions with explicit type hints.

--- app/api/errors.py
+++ app/api/errors.py
@@ -3,26 +3,26 @@ from fastapi import HTTPException, status
 from app.core.loging import logger
 
 
-def error_401(detail: str):
+def error_401(detail: str) -> None:
     logger.error(detail)
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail)
 
 
-def error_403(detail: str):
+def error_403(detail: str) -> None:
     logger.error(detail)
     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=detail)
 
 
-def error_404(detail: str):
+def error_404(detail: str) -> None:
     logger.error(detail)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
 
 
-def error_409(detail: str):
+def error_409(detail: str) -> None:
     logger.error(detail)
     raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=detail)
 
 
-def error_500(detail: str):
+def error_500(detail: str) -> None:
     logger.critical(detail)
     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=detail)
